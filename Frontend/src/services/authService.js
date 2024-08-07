178aa5849ac95257990af5e797673f0cd54fc8ef
1uhis <1x21bessu@gmail.com>
2024-08-07 16:36:52 +1200

    debug

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,11 +1,12 @@
 import axiosApiInstance from '../utils/axiosInstance';
 import { tokenStorage } from '../utils/localStorage';
+import { API_PATH } from '../utils/urlRoutes';
 
 console.log('Stored Token:', tokenStorage.get());
 
 export const login = async (email, password) => {
   try {
-    const url = '/auth/login';
+    const url = API_PATH.auth.login;
     console.log('Request URL:', axiosApiInstance.defaults.baseURL + url);
     const response = await axiosApiInstance.post(url, { email, password });
     // Save the token to localStorage
