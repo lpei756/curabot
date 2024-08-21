0e2c730a2107dc7acce56a52a968efb082d53bd5
Lei Pei <126681625+lpei756@users.noreply.github.com>
2024-08-21 16:59:16 +1200

    adminregister and adminlogin endpoint

--- Backend/src/routes/doctorRoutes.js
+++ Backend/src/routes/doctorRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getDoctorById, getDoctorsByClinic } from '../controllers/doctorController.js';
+import { adminRegister, adminLogin } from '../controllers/adminController.js';
 import { DOCTOR_PATHS } from './path.js';
 
 const router = express.Router();
@@ -7,5 +8,11 @@ const router = express.Router();
 router.get(DOCTOR_PATHS.read, getDoctorById);
 
 router.get(DOCTOR_PATHS.clinicDoctors, getDoctorsByClinic);
+
+// 医生注册
+router.post(DOCTOR_PATHS.doctorRegister, adminRegister);
+
+// 医生登录
+router.post(DOCTOR_PATHS.doctorLogin, adminLogin);
 
 export default router;
