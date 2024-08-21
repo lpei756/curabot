35269df1f7089b185adfcd8d00eb5b71fa750422
1uhis <1x21bessu@gmail.com>
2024-08-22 11:48:44 +1200

    tidy up code

--- Frontend/src/services/doctorService.js
+++ Frontend/src/services/doctorService.js
@@ -1,3 +1,4 @@
+import axiosApiInstance from '../utils/axiosInstance';
 import { API_PATH } from '../utils/urlRoutes';
 
 export const getDoctorsByClinic = async (clinicId) => {
@@ -13,6 +14,16 @@ export const getDoctorsByClinic = async (clinicId) => {
     return response.json();
   } catch (error) {
     console.error('Error fetching doctors:', error.message);
+    throw error;
+  }
+};
+
+export const getDoctorById = async (doctorId) => {
+  try {
+    const response = await axiosApiInstance.get(API_PATH.doctor.read.replace(':doctorID', doctorId));
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching doctor:', error);
     throw error;
   }
 };
