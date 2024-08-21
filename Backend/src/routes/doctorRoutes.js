0dee4dfdcbe86fe8fc92afa141bb3fba16bf4913
Lei Pei <126681625+lpei756@users.noreply.github.com>
2024-08-21 17:06:27 +1200

    resolve conflicts

--- Backend/src/routes/doctorRoutes.js
+++ Backend/src/routes/doctorRoutes.js
@@ -9,10 +9,8 @@ router.get(DOCTOR_PATHS.read, getDoctorById);
 
 router.get(DOCTOR_PATHS.clinicDoctors, getDoctorsByClinic);
 
-// 医生注册
 router.post(DOCTOR_PATHS.doctorRegister, adminRegister);
 
-// 医生登录
 router.post(DOCTOR_PATHS.doctorLogin, adminLogin);
 
 export default router;
