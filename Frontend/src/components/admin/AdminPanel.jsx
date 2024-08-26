76dee4d8b28d91605cbb9aec5799b244fb2f9828
Lei Pei <126681625+lpei756@users.noreply.github.com>
2024-08-26 15:11:44 +1200

    update botton color

--- Frontend/src/components/admin/AdminPanel.jsx
+++ Frontend/src/components/admin/AdminPanel.jsx
@@ -86,9 +86,14 @@ const AdminPanel = () => {
                 </TableContainer>
             </Box>
 
-            <Button variant="contained" color="secondary" onClick={handleLogout}>
+            <Button
+                variant="contained"
+                style={{ backgroundColor: '#03035d', color: '#fff' }}
+                onClick={handleLogout}
+            >
                 Logout
             </Button>
+
         </Box>
     );
 };
