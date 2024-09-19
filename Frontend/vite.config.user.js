9223dd8ac9f8505f0a5e05bc40e64321a8c91ba3
Lei Pei <126681625+lpei756@users.noreply.github.com>
2024-09-19 18:38:48 +1200

    update 3002

--- Frontend/vite.config.user.js
+++ Frontend/vite.config.user.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
+/* global process */
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173
-  },
-  envPrefix: 'VITE_',
-});
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd(), '')};
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 5173
+    },
+    envPrefix: 'VITE_',
+  });
+};
