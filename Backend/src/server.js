674f3538b1c649962260ed6c3dc8f8f6dbe78e20
Lei Pei <126681625+lpei756@users.noreply.github.com>
2024-09-19 20:23:27 +1200

    update server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,20 +1,11 @@
+import dotenv from 'dotenv';
 import connectDB from './config/database.js';
 import app from './app.js';
 import './services/scheduler.js';
 
-if (process.env.NODE_ENV !== 'production') {
-    import('dotenv').then(dotenv => {
-        dotenv.config();
-        console.log("Development environment variables loaded");
-    }).catch(error => {
-        console.error("Failed to load dotenv", error);
-    });
-}
+dotenv.config();
 
-connectDB()
-    .then(() => console.log("Database connected successfully"))
-    .catch((error) => console.error("Database connection failed:", error));
+connectDB();
 
-const PORT = process.env.PORT || 3002;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
