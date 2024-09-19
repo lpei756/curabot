d7e3f8a264a8e44afea6565832d23f67b568b1cf
Lei Pei <126681625+lpei756@users.noreply.github.com>
2024-09-19 19:11:54 +1200

    update index

--- Frontend/vite.config.user.js
+++ Frontend/vite.config.user.js
@@ -1,9 +1,8 @@
-/* global process */
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd(), '')};
+  const env = loadEnv(mode, process.cwd(), '');
 
   return defineConfig({
     plugins: [react()],
@@ -11,5 +10,8 @@ export default ({ mode }) => {
       port: 5173
     },
     envPrefix: 'VITE_',
+    define: {
+      'process.env': env,  // 通过 define 注入全局的环境变量
+    },
   });
 };
