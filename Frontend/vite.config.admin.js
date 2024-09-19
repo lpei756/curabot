bd88476f9f97214c7f97986ebcd1ca684c15a214
Lei Pei <126681625+lpei756@users.noreply.github.com>
2024-09-19 19:11:54 +1200

    update index

--- Frontend/vite.config.admin.js
+++ Frontend/vite.config.admin.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
+/* global process */
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5174
-  },
-  envPrefix: 'VITE_',
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 5174, // 不同的端口用于区分不同环境
+    },
+    envPrefix: 'VITE_',
+    define: {
+      'process.env': env,
+    },
+    build: {
+      outDir: 'dist/admin', // 构建输出目录为 admin 文件夹
+    },
+  });
+};
--- Frontend/vite.config.superadmin.js
+++ Frontend/vite.config.superadmin.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite';
+/* global process */
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5175
-  },
-  envPrefix: 'VITE_',
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 5175, // 不同的端口用于超级管理员
+    },
+    envPrefix: 'VITE_',
+    define: {
+      'process.env': env,
+    },
+    build: {
+      outDir: 'dist/superadmin', // 构建输出目录为 superadmin 文件夹
+    },
+  });
+};
